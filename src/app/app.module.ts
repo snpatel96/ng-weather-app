024f3d5f0f190c13e0cc509225a9ee78cf217b84
snehal patel <snpatel.dev@gmail.com>
2023-01-07 15:05:01 -0500

    #1 components, service & styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule
